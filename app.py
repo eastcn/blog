77def243524c43057eb8c49eeed5c52b0caa5ab0
east <417034606@qq.com>
2019-11-03 10:34:22 +0800

    支持跨域

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from flask_cors import CORS
 from factory import create_app
 
 app = create_app()
+CORS(app, origins="*", supports_credentials=True)
+
 
 @app.route('/hello')
 def hello():
     return "hello"
 
 if __name__ == '__main__':
-    CORS(app, resources={r"/*": {"origins": "*"}})
     app.run(port=8089)
