bde0e9471aaad0ee713536f5af42777b1d4e5553
east <417034606@qq.com>
2019-11-05 18:20:28 +0800

    修复图片问题

--- config.py
+++ config.py
@@ -6,5 +6,7 @@
 
 
 class CONFIG:
+    ENV = "LOCAL"
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:EASTlove7!@47.111.163.9:3306/blog"
     SQLALCHEMY_DATABASE_URI_SNEAKER = "mysql+pymysql://root:EASTlove7!@47.111.163.9:3306/sneakerlover"
+
--- controller/Aticle.py
+++ controller/Aticle.py
@@ -5,6 +5,7 @@
 """
 import json
 import datetime
+import socket
 import time
 import traceback
 
@@ -63,13 +64,19 @@ def del_article():
 
 @article.route('/uploadImage', methods=['post'])
 def uploadImage():
+
+    if (request.remote_addr == '127.0.0.1'):
+        myaddr = '127.0.0.1'
+    else:
+        myname = socket.getfqdn(socket.gethostname())
+        myaddr = socket.gethostbyname(myname)
     result = {}
     try:
         f = request.files['image']
         file_name = f.filename.split('.')
         file_url = f'/static/{f.filename[0]}_{str(int(time.time()))}.{file_name[1]}'
         f.save(f'.{file_url}')
-        result['url'] = 'http://localhost:8089' + file_url
+        result['url'] =  f'http://{myaddr}:8089{file_url}'
         result['success'] = True
     except Exception:
         traceback.print_exc()
