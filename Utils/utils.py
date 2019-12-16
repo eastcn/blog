f0ad4a29e06cd23c38a0fe69f87adced688e008f
hefeng <hefeng@wacai.com>
2019-12-16 15:34:15 +0800

    Moive模块： 1. 爬虫 ✅ 2. 数据入库 ✅ 3. 数据读取+前端调试 ❌ 4. 代理 ✅

--- Utils/utils.py
+++ Utils/utils.py
@@ -3,6 +3,11 @@ date:
 author:
 function:
 """
+import json
+
+import requests
+
+from config import CONFIG
 
 
 def true_or_false(word):
@@ -13,3 +18,20 @@ def true_or_false(word):
     if word is True:
         res = 0
     return res
+
+
+def get_ip_proxy(count):
+    """
+    获取讯代理的长效ip
+    """
+    print("获取ip代理")
+    spider_id = CONFIG.PROXY_USER_ID
+    order_no = CONFIG.PROXY_ORDER_NO
+    url = f"http://api.xdaili.cn/xdaili-api/greatRecharge/getGreateIp?{spider_id}&ordernoo={order_no}&returnType=2&count={count}"
+    result = json.loads(requests.get(url).text)
+    print(f"获取到的代理结果为：{result}")
+    if result['ERRORCODE'] == "0":
+        ip_list = result['RESULT']
+        return ip_list
+    else:
+        return []
